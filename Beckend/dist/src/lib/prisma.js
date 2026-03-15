990b6226b1b45b738c04c77089781168e89e213b
Barzu <barzupano@gmail.com>
2026-03-15 12:43:44 +0800

    remove prisma build step

--- Beckend/dist/src/lib/prisma.js
+++ Beckend/dist/src/lib/prisma.js
@@ -1,13 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
-const adapter_pg_1 = require("@prisma/adapter-pg");
 const client_1 = require("@prisma/client");
-const env_1 = require("../config/env");
-const adapter = new adapter_pg_1.PrismaPg({ connectionString: env_1.env.DATABASE_URL });
 exports.prisma = global.__prisma ??
     new client_1.PrismaClient({
-        adapter,
         log: ["warn", "error"],
     });
 if (process.env.NODE_ENV !== "production") {
--- Beckend/src/lib/prisma.ts
+++ Beckend/src/lib/prisma.ts
@@ -1,18 +1,13 @@
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-import { env } from "../config/env";
 
 declare global {
   // eslint-disable-next-line no-var
   var __prisma: PrismaClient | undefined;
 }
 
-const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
-
 export const prisma =
   global.__prisma ??
   new PrismaClient({
-    adapter,
     log: ["warn", "error"],
   });
 
